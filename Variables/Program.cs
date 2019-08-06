c7441d8844d66a4b9fb96233aaa8e75315097762
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 09:48:30 +0100

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a man named George");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name George");
+            Console.WriteLine("But didn't like being 70");
+
+            Console.ReadLine();
+        }
+    }
+}
